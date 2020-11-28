9d97e7984d5f988bd632f9bef84880129896fad9
Irina-Koksharova <biodgerelo@ukr.net>
2020-11-28 16:58:05 +0200

    Добавляет компонент Container

--- src/App.jsx
+++ src/App.jsx
@@ -6,22 +6,23 @@ import FriendList from './components/FriendList/FriendList';
 import friends from './components/FriendList/friends.json';
 import TransactionHistory from './components/TransactionHistory/TransactionHistory';
 import transactions from './components/TransactionHistory/transactions.json';
+import Container from './components/Container/Container';
 
 function App() {
-    return <div>
-        <Profile
-            name={user.name}
-            tag={user.tag}
-            location={user.location}
-            avatar={user.avatar}
-            stats = {user.stats} 
-        />
-        <Statistics stats={statisticalData}
-        />
-        <FriendList friends={friends}
-        />
-        <TransactionHistory items={transactions}/>
-    </div>;
+  return (
+    <Container>
+      <Profile
+        name={user.name}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics stats={statisticalData} />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
+    </Container>
+  );
 }
 
 export default App;

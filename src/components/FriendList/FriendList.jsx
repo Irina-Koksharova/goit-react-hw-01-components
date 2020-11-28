84dcc331a7b8626c9de5750520937c64a783dc24
Irina-Koksharova <biodgerelo@ukr.net>
2020-11-28 21:17:36 +0200

    Добавляет стили для компонента FriendsList

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,27 +1,29 @@
 import PropTypes from 'prop-types';
 import FriendListItem from './FriendListItem/FriendListItem';
+import s from './FriendList.module.css';
 
-function FriendList({friends}) {
-    return <ul className="friend-list">
-        {friends.map(friend => (
-            <li className="item" key={friend.id}>
-                <FriendListItem
-                    avatar={friend.avatar}
-                    name={friend.name}
-                    isOnline={friend.isOnline}
-                />
-            </li>
-        ))}
+function FriendList({ friends }) {
+  return (
+    <ul className={s.list}>
+      {friends.map(friend => (
+        <li className={s.item} key={friend.id}>
+          <FriendListItem
+            avatar={friend.avatar}
+            name={friend.name}
+            isOnline={friend.isOnline}
+          />
+        </li>
+      ))}
     </ul>
+  );
 }
 
 FriendList.propTypes = {
-    friends: PropTypes.arrayOf(
+  friends: PropTypes.arrayOf(
     PropTypes.shape({
       id: PropTypes.number.isRequired,
     }),
   ),
-}
+};
 
 export default FriendList;
-    
--- src/components/FriendList/FriendListItem/FriendListItem.jsx
+++ src/components/FriendList/FriendListItem/FriendListItem.jsx
@@ -1,18 +1,21 @@
 import { Fragment } from 'react';
 import PropTypes from 'prop-types';
+import s from './FriendListItem.module.css';
 
 function FriendListItem({ avatar, name, isOnline }) {
-    return <Fragment>
-        <span className="status">{isOnline}</span>
-        <img className="avatar" src={avatar} alt={name} width="48" />
-        <p className="name">{name}</p>
-    </Fragment> 
+  return (
+    <Fragment>
+      <span className={isOnline ? s.online : s.offline}></span>
+      <img className={s.avatar} src={avatar} alt={name} width="48" />
+      <p className={s.name}>{name}</p>
+    </Fragment>
+  );
 }
 
 FriendListItem.propTypes = {
-    name: PropTypes.string.isRequired,
-    avatar: PropTypes.string.isRequired,
-    isOnline: PropTypes.bool.isRequired,
-}
+  name: PropTypes.string.isRequired,
+  avatar: PropTypes.string.isRequired,
+  isOnline: PropTypes.bool.isRequired,
+};
 
 export default FriendListItem;
